cd05b97dfd7455033aacc4f9aa42d70262c2135e
Denis KOTLYAR <dkotlyar@e1r10p1.unit.ua>
2018-07-06 17:27:06 +0300

    surely finished

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dkotlyar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 14:11:28 by dkotlyar          #+#    #+#             */
-/*   Updated: 2018/07/06 11:30:43 by dkotlyar         ###   ########.fr       */
+/*   Updated: 2018/02/22 18:35:40 by dkotlyar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "libft.h"
 # define MCH(x) if (!(x)) return (-1);
 # define RET_TRUE(x) if (x) return (1);
-# define BUFF_SIZE 8
+# define BUFF_SIZE 2
 # define ENDL '\n'
 # define END '\0'
 
